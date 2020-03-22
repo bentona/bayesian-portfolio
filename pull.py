cc6ccb71688576a4276ce95f0c5a2ece5ba5591d
Benton <benton.anderson@gmail.com>
2020-03-21 20:51:59 -0500

    generate random samples

--- pull.py
+++ pull.py
@@ -1,7 +1,9 @@
 import requests
-import datetime
+from datetime import datetime
+from datetime import timedelta
 import pandas as pd
 import sys
+from random import sample
 
 import settings
 
@@ -12,37 +14,54 @@ def fetch_range(symbol, start, end):
         'resolution': 'D', # we only need per-day data
         'from': date_to_timestamp(start),
         'to': date_to_timestamp(end),
-        'token': settings.FINNHUB_API_KEY
     }
-    return request(params)
+    return request('stock/candle', params)
 
 def date_to_timestamp(date_string):
-    date = datetime.datetime.strptime(date_string, "%m-%d-%Y")
-    stamp = datetime.datetime.timestamp(date) 
+    date = datetime.strptime(date_string + "-+0000", "%m-%d-%Y-%z")
+    stamp = datetime.timestamp(date) 
     return int(stamp)
 
-def request(params):
-    r = requests.get('https://finnhub.io/api/v1/stock/candle', params=params)
+def request(endpoint, params):
+    auth_param = {'token': settings.FINNHUB_API_KEY}
+    params_with_token = {**params, **auth_param }
+    r = requests.get('https://finnhub.io/api/v1/' + endpoint, params=params_with_token)
     print("DEBUG: Request: " + r.url)
     return r.json()
 
-def format(results):
+def to_df(results, symbol):
     data = {
         'low': results['l'],
         'high': results['h'],
         'volume': results['v'],
-        'timestamp': results['t']
+        'timestamp': results['t'],
+        'datetime': pd.to_datetime(results['t'], unit='s'),
+        'symbol': symbol
     }
     return pd.DataFrame(data=data)
+
+def all_symbols():
+    return request('stock/symbol', {'exchange': 'US'})
 
 def run(args):
     try:
         symbol = args[1]
         start = args[2]
         end = args[3]
-        return format(fetch_range(symbol=symbol, start=start, end=end))
+        return to_df(fetch_range(symbol=symbol, start=start, end=end), symbol=symbol)
     except Exception:
         print("\nUsage example: pull.py AAPL 01-15-2020 02-25-2020 \n")
         raise
 
-print(run(sys.argv))
+
+#import code; code.interact(local=dict(globals(), **locals()))
+
+
+def random_sample(n):
+    start = '01-01-2000'
+    end = '01-01-2020'
+    symbols = [ r['symbol'] for r in sample(all_symbols(), n) ]
+    dfs = [ to_df(fetch_range(symbol=symbol, start=start, end=end), symbol=symbol) for symbol in symbols]
+    return pd.concat(dfs)
+
+random_sample(10).to_csv('sample.csv')
