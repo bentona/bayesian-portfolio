7fea1f8dd252c9224bf9af8bda64708387354ace
bentona <benton.anderson@gmail.com>
2020-04-23 23:37:18 -0500

    test against current data

--- simple-jags.R
+++ simple-jags.R
@@ -11,6 +11,7 @@ S <- 10000
 port_samples <- matrix(NA,S,3)
 
 T <- nrow(daily_return)
+N <- nrow(by_symbol[[1]])
 
 # Initial Values
 mu1 <- colMeans(daily_return)
@@ -85,4 +86,19 @@ optimal <- optimization$par
 normalized_optimal <- optimal/sum(optimal)
 
 normalized_optimal
+
+
+test_sample <- load_data('./test.csv')
+test_by_symbol <- group_by_symbol(sample)
+test_daily_return <- daily_returns(by_symbol)
+
+
+stock_values <- matrix(ncol=Nstocks, nrow=nrow(test_daily_return))
+stock_values[1,] <- normalized_optimal
+for (i in 1:nrow(stock_values)){
+  stock_values[i+1,] <- (test_daily_return[i,]/100 + 1) * stock_values[i,]
+}
+
+sum(stock_values[nrow(stock_values),])
+
 
